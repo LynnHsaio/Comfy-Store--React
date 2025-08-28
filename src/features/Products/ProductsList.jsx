b721aae24783fe9a9426cce325403cbbdef9fbcd
Lilin <ihsetheyme@gmail.com>
2025-08-28 16:50:59 +0800

    [IMP] 如果沒有商品顯示文字

--- src/features/Products/ProductsList.jsx
+++ src/features/Products/ProductsList.jsx
@@ -2,6 +2,8 @@ import ProductItem from "./ProductItem";
 import styles from "./ProductsList.module.scss";
 
 export default function ProductsList({ list, layout = "grid" }) {
+  if (!list || !list.length) return <h5>Sorry, there are no products</h5>;
+
   return (
     <ul className={`${styles.ProductsList} ${styles[layout]}`}>
       {list.map((item) => (
