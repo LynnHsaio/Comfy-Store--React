086e0bcb7216c8ebeb54737d21c0afbbd6ba4426
Lilin <ihsetheyme@gmail.com>
2025-09-01 23:08:03 +0800

    [ADD] Hero加入樣式

--- src/features/Home/Hero.jsx
+++ src/features/Home/Hero.jsx
@@ -1,22 +1,27 @@
 import { NavLink } from "react-router-dom";
 import Gallery from "../../ui/Gallery";
+import styles from "./Hero.module.scss";
 
 export default function Hero() {
   return (
-    <div>
-      <div>
-        <h1>We are changing the way people shop</h1>
+    <div className={styles.hero}>
+      <div className={styles.columnLeft}>
+        <h1 className="text-4xl">We are changing the way people shop</h1>
 
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
           repellat explicabo enim soluta temporibus asperiores aut obcaecati
           perferendis porro nobis.
         </p>
+
         <div>
-          <NavLink to="/products">Our Products</NavLink>
+          <NavLink to="/products" className="btn-primary">
+            Our Products
+          </NavLink>
         </div>
       </div>
-      <div style={{ width: "50%", height: "20rem" }}>
+
+      <div className={styles.columnRight}>
         <Gallery />
       </div>
     </div>
