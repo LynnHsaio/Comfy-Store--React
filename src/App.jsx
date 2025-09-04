96bfa3d79f8fe5b5cd3f8a937cbf370f086ff3e7
Lilin <ihsetheyme@gmail.com>
2025-09-04 23:42:17 +0800

    [ADD] 加入lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Products from "./pages/Products";
-import Cart from "./pages/Cart";
-import CheckOut from "./pages/CheckOut";
+import { Suspense, lazy } from "react";
 import AppLayout from "./ui/AppLayout";
-import Product from "./pages/Product";
-import PageNotFound from "./pages/PageNotFound";
+import Loading from "./ui/Loading";
+
+// lazy import pages
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Products = lazy(() => import("./pages/Products"));
+const Cart = lazy(() => import("./pages/Cart"));
+const CheckOut = lazy(() => import("./pages/CheckOut"));
+const Product = lazy(() => import("./pages/Product"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,17 +27,19 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="products" element={<Products />} />
-            <Route path="products/:id" element={<Product />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="checkout" element={<CheckOut />} />
-          </Route>
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Home />} />
+              <Route path="about" element={<About />} />
+              <Route path="products" element={<Products />} />
+              <Route path="products/:id" element={<Product />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="checkout" element={<CheckOut />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </QueryClientProvider>
   );
