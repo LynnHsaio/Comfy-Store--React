4c15273f940cd571c1343e938ec09ccb65b52c74
Lilin <ihsetheyme@gmail.com>
2025-09-05 18:27:27 +0800

    [ADD] ProductList加入list排版

--- src/features/Products/ProductItem.jsx
+++ src/features/Products/ProductItem.jsx
@@ -3,7 +3,7 @@ import { formatCurrency } from "../../utils/helpers";
 import styles from "./ProductItem.module.scss";
 
 export default function ProductItem({ item, layout = "grid" }) {
-  const { image, title, price } = item.attributes;
+  const { image, title, company, price } = item.attributes;
 
   return (
     <Link
@@ -14,7 +14,10 @@ export default function ProductItem({ item, layout = "grid" }) {
         <img src={image} alt={title} />
       </figure>
       <div className={styles.textContainer}>
-        <h3>{title}</h3>
+        <div>
+          <h3>{title}</h3>
+          <h4 className={styles.company}>{company}</h4>
+        </div>
         <p className="text-primary">{formatCurrency(price)}</p>
       </div>
     </Link>
